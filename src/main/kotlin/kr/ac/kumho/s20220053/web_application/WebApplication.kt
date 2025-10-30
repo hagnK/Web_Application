655828be5283db5c61f34d4e03eef773735e5e2d
Koohyeonkyu <akddy3@gmail.com>
2025-10-30 16:32:29 +0900

    🎉프로젝트 시작

---
+++ src/main/kotlin/kr/ac/kumho/s20220053/web_application/WebApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumho.s20220053.web_application
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumho/s20220053/web_application/WebApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumho.s20220053.web_application
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
